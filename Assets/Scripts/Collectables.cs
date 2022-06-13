0449e1da5b51e2439409f7d24674e32284f03d4a
Antía Herrero Gabiola <hergabant@IESPIRINGALLA.AD>
2022-06-13 20:32:53 +0200

    enemigos y coleccionables

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
+    public float speed = 4.5f;
+    public float points;
+    private Rigidbody2D rb;
+    private float limit = -7.32f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < limit){
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -5,24 +5,50 @@ using UnityEngine;
 public class SpawnObjects : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    public GameObject collectablePrefab;
-    public float respawnTime = 1.0f;
+    public GameObject collectablePrefab10;
+    public GameObject collectablePrefab20;
     private float screenLimit = 4.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(EnemyWave());
+        StartCoroutine(CollectableWave10());
+        StartCoroutine(CollectableWave20());
     }
 
+//Enemigos
     private void SpawnEnemy(){
         GameObject newEnemy = Instantiate(enemyPrefab) as GameObject;
         newEnemy.transform.position = new Vector2(8f, Random.Range(-screenLimit, screenLimit));
     }
     IEnumerator EnemyWave(){
         while(true){
-            yield return new WaitForSeconds(respawnTime);
+            yield return new WaitForSeconds(1f);
             SpawnEnemy();
         }
-        
+    }
+
+//Coleccionables
+    private void SpawnCollectable10(){
+        GameObject newCollectable10 = Instantiate(collectablePrefab10) as GameObject;
+        newCollectable10.transform.position = new Vector2(8f, Random.Range(-screenLimit, screenLimit));
+    }
+    IEnumerator CollectableWave10(){
+         while(true){
+            yield return new WaitForSeconds(1.5f);
+            SpawnCollectable10();
+        }
+    }
+    private void SpawnCollectable20(){
+        GameObject newCollectable20 = Instantiate(collectablePrefab20) as GameObject;
+        newCollectable20.transform.position = new Vector2(8f, Random.Range(-screenLimit, screenLimit));
+    }
+
+    IEnumerator CollectableWave20(){
+        while(true){
+            yield return new WaitForSeconds(4f);
+            SpawnCollectable20();
+        }
     }
 }
