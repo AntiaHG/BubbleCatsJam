65ea63dd2b914f29534d29043419903f58bc450d
Antía Herrero Gabiola <hergabant@IESPIRINGALLA.AD>
2022-06-15 20:41:42 +0200

    vidas sprites

--- Assets/Scripts/Anotaciones.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Anotaciones : MonoBehaviour
-{
-    //Hacer que no colisionen los enemigos con las estrellitas(que se ignoren)
-}
